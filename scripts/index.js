28d373c9ca7aede1a470269aa4626ad2f0c8c6d5
Erzhan Turgunbaev <nee.klaus@yandex.ru>
2025-05-02 01:32:40 +0700

    1-try

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,46 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const formButton = document.querySelector('.card_create');
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function render() {
+  initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.append(cardElement);
+  });
+}
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+// Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  // Клонируем шаблон
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  // Находим элементы
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  const likeButton = cardElement.querySelector('.card__button-like');
 
-// @todo: Вывести карточки на страницу
+  // Заполняем данные
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  // Обработчик удаления 
+  deleteButton.addEventListener('click', () => {
+    deleteCallback(cardElement); 
+  });
+
+
+
+
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
+}
+
+
+render();
